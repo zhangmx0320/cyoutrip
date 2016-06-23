d092a58c5034d3cf580c4f9ae956218a84684a50
zhangmx <zhangmx0320@gmail.com>
2016-06-23 21:58:55 +0800

    login

---
+++ app/src/main/java/com/cyoutrip/base/BaseActivity.java
@@ -0,0 +1,62 @@
+package com.cyoutrip.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+
+import com.cyoutrip.R;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by Administrator on 2016/6/22.
+ */
+public class BaseActivity extends AppCompatActivity {
+
+    protected ActionBar mActionBar;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutId());
+
+        mActionBar = getSupportActionBar();
+        if (hasActionBar()) {
+            initActionBar(mActionBar);
+        }
+        ButterKnife.bind(this);
+    }
+
+    protected  int getLayoutId(){
+        return 0;
+    }
+
+    protected boolean hasBackButton(){
+        return false;
+    }
+
+    protected boolean hasActionBar() {
+        return getSupportActionBar() != null;
+    }
+
+    protected int getActionBarTitle() {
+        return R.string.app_name;
+    }
+
+    protected void initActionBar(ActionBar actionBar) {
+        if (actionBar == null)
+            return;
+        if (hasBackButton()) {
+            mActionBar.setDisplayHomeAsUpEnabled(true);
+            mActionBar.setHomeButtonEnabled(true);
+        } else {
+            actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_TITLE);
+            actionBar.setDisplayUseLogoEnabled(false);
+            int titleRes = getActionBarTitle();
+            if (titleRes != 0) {
+                actionBar.setTitle(titleRes);
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/cyoutrip/ui/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.cyoutrip.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cyoutrip.R;
+import com.cyoutrip.base.BaseActivity;
+
+public class LoginActivity extends BaseActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_login;
+    }
+}
